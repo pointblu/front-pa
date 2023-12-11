a72f8e304f920364c2a23f543dd83a93f505c34d
pointblu <puntoazulpanaderia@gmail.com>
2023-12-11 12:28:41 -0500

    fix: Corrección de error 404 en enrutamiento github pages

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { FiltersProvider } from "./context/filters";
 import { AuthProvider } from "./auth/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <FiltersProvider>
     <AuthProvider>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter basename={process.env.PUBLIC_URL}>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </AuthProvider>
   </FiltersProvider>
 );
